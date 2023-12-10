0bedb2a5ad05a9e4b80dc5805d27b965825018f6
Steve <steven4.lewis@live.uwe.ac.uk>
2023-12-10 23:54:22 +0000

    Scene Transitions and other bits are working

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //Public variables to access scene load from any other script - might need in gamne manager?
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    //Function to load the scene
+    public void LoadNextScene()
+    { 
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
-    // Update is called once per frame
-    void Update()
+    //Courotine To delay the Scene Load - Animation has to play first
+    IEnumerator LoadLevel(int levelIndex)
     {
-        
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+
+        //SceneManager.LoadScene("Scene B");   -Can also do this :)
     }
 }
